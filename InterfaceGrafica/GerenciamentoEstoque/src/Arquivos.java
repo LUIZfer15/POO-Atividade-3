c5a1374a231fd98986a10d3ee7181391a5eea175
LUIZfer15 <101845394+LUIZfer15@users.noreply.github.com>
2023-06-30 17:04:02 -0300

    Add files via upload

--- InterfaceGrafica/GerenciamentoEstoque/src/Arquivos.java
+++ InterfaceGrafica/GerenciamentoEstoque/src/Arquivos.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -112,7 +114,7 @@ public class Arquivos {
         try {
             File file = new File(NOME_ARQUIVO_BINARIO);
             inputStream = new FileInputStream(file);
-            objectStream = new ObjectOutputStream(outputStream);
+            objectStream = new ObjectInputStream(inputStream);
             produto = (Produto) objectStream.readObject();
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Erro ao carregar o veiculo do arquivo binário!");
--- InterfaceGrafica/GerenciamentoEstoque/src/TelaInicial.java
+++ InterfaceGrafica/GerenciamentoEstoque/src/TelaInicial.java
@@ -1,3 +1,24 @@
-public class TelaInicial {
-    
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TelaInicial extends JFrame {
+
+    private JPanel panel;
+
+    public TelaInicial() {
+        setTitle("Manipulação de Arquivos");
+        setLayout(new FlowLayout());
+
+        this.panel = new JPanel();
+        this.panel.setLayout(new FlowLayout());
+        this.panel.setPreferredSize(new Dimension(500, 500));
+        add(this.panel);
+
+        setSize(new Dimension(500, 500));
+        setPreferredSize(new Dimension(500, 500));
+    }
+
 }
